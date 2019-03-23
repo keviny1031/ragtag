77f3255006072e42a0666c53abb8a1f39cdc3bf8
Filipi Bardusco <filipi@bardusco.com>
2019-03-23 18:28:17 -0400

    Add files via upload

--- RickTheRocket.py
+++ RickTheRocket.py
@@ -17,18 +17,31 @@ init()
 size=(960,720)
 screen=display.set_mode(size)
 fps=time.Clock()
+font = font.SysFont('Arial', 30)
 display.set_caption("Rick The Rocket")
-xSpeed = 0
-ySpeed = 0
-angle = 45
+CHARRAD = 22
+angle = 0
+ang1 = pi/4
+ang2 = 3 * pi / 4
 launchSpeed = 5
-maxJumps = 1
+maxJumps = 2
 curJumps = maxJumps
 onGround = True
 GRAVITY = 1
+xSpeed = 0
+ySpeed = 0
 xPos = 0
-yPos = 0
+yPos = 720 - CHARRAD
 ##################
+
+def rot_center(image, angle):
+    """rotate an image while keeping its center and size"""
+    orig_rect = image.get_rect()
+    rot_image = transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
 
 def jump():
     #Jumps based on the current angle of the rocket
@@ -40,44 +53,48 @@ def jump():
         onGround = False
         curJumps -= 1
         ySpeed -= launchSpeed * 10 * round(sin(angle + (pi / 2)), 10) #Minus for PyGame heights being inverted
-        xSpeed += launchSpeed * round(cos(angle + (pi / 2)), 10)
+        xSpeed += launchSpeed * round(cos(angle + (pi / 2)), 10)#Rounds to avoid a pi decimal error
 
-def menu():
+
+
+def game():
     global xPos
     global yPos
     global xSpeed
     global ySpeed
     global onGround
     global launchSpeed
+    global angle
+    global ang1
+    global ang2
     running = True
     fps = time.Clock()
     
     while running:
-        print(round(sin(angle + (pi / 2)), 10), xPos, yPos)
         for evnt in event.get():
             if evnt.type == QUIT:
                 return "quit"
             if evnt.type == KEYDOWN:
                 if evnt.key == K_SPACE:
                     jump()
-        
+        print(xPos, yPos)
         keys = key.get_pressed()
-        if keys[K_UP]:
-            chary -= 0.25
-        if keys[K_DOWN]:
-            chary += 0.25
-        if keys[K_RIGHT] and (charx + 50) < 960:
-            charx += 0.25
-        if keys[K_LEFT] and charx > 0:
-            charx -= 0.25
-
+        if keys[K_LEFT]:
+            angle += 0.01
+            ang1 += 0.01
+            ang2 += 0.01
+        elif keys[K_RIGHT]:
+            angle -= 0.01
+            ang1 -= 0.01
+            ang2 -= 0.01
+        
         xPos += xSpeed
         yPos += ySpeed
         
-        if yPos >= 0:
+        if yPos >= 700 - CHARRAD:
             onGround = True
             ySpeed = 0
-            yPos = 0
+            yPos = 700 - CHARRAD
             
         if onGround:
             refreshJumps()
@@ -85,6 +102,9 @@ def menu():
             ySpeed = 0
         else:
             ySpeed += GRAVITY
+
+        ###########################
+        
     
 def refreshJumps():
     global maxJumps
@@ -92,10 +112,10 @@ def refreshJumps():
     curJumps = maxJumps
     
 
-page = "menu"
+page = "game"
 while page != "quit":
-    if page == "menu":
-        page = menu()
+    if page == "game":
+        page = game()
 
     
 
