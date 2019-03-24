38837a2f40a5ef4f264506ba691154d400cbd9b4
Filipi Bardusco <filipi@bardusco.com>
2019-03-24 00:39:58 -0400

    Add files via upload

--- RickTheRocket.py
+++ RickTheRocket.py
@@ -9,7 +9,7 @@ from math import *
 #Math
 from random import *
 #Random
-from rocket import *
+from Rocket import *
 #############
 
 
@@ -25,7 +25,38 @@ CHARRAD = 22
 launchSpeed = 11
 GRAVITY = 0.38
 sprite = transform.scale(image.load("main1.png"), (40,40))
+buttonImg = transform.scale(image.load("buttonImg.png"), (60,20))
+jumpUpImg = transform.scale(image.load("jumpUp.png"), (20,20))
+speedUpImg = transform.scale(image.load("speedUp.png"), (20,20))
 ##################
+
+class doorButton:
+    def __init__(bX, bY, bDir, dX, dY, dW, dH, speed):
+        self.bX = bX
+        self.bY = bY
+        self.bDir = bDir
+        self.door = Rect(dX, dY, dW, dH)
+        self.speed = speed
+
+    def pressed(rocket):
+        if rocket.speed() >= speed:
+            del self.door
+
+class powerup:
+    def __init__(xPos, yPos, ability):
+        hitbox = Rect(xPos, yPos, 20, 20)
+        self.ability = ability
+        if ability == "jumpUp":
+            self.img = jumpUpImg
+        elif ability == "speedUp":
+            self.img = speedUpImg
+
+    def collected(rocket):
+        if ability == "jumpUp":
+            rocket.jumpsUp()
+        elif ability == "launchUp":
+            rocket.changeSpeed(rocket.launchSpeed())
+    
 
 def rot_center(image, angle):
     """rotate an image while keeping its center and size"""
@@ -67,17 +98,18 @@ def game():
             rick.refreshJumps()
             rick.xSpeed = 0
             rick.ySpeed = 0
+            rick.bSpeed = 0
         else:
             rick.ySpeed += GRAVITY
 
         ###########################
-        screen.fill((0, 0, 0))
+        screen.fill((0, 0, 0)) 
         #Hitbox    
         draw.circle(screen, (0,0,0), (int(rick.xPos), int(rick.yPos)), 22, 1)
 
         #Points of arc
-        draw.circle(screen,(255,0,0),(int(rick.xPos + (22 * cos(rick.ang1))), int(rick.yPos - (22 * sin(rick.ang1)))), 3, 0)
-        draw.circle(screen,(255,0,0),(int(rick.xPos + (22 * cos(rick.ang2))), int(rick.yPos - (22 * sin(rick.ang2)))), 3, 0)
+        draw.circle(screen,(255,0,0), (int(rick.xPos + (22 * cos(rick.ang1))), int(rick.yPos - (22 * sin(rick.ang1)))), 3, 0)
+        draw.circle(screen,(255,0,0), (int(rick.xPos + (22 * cos(rick.ang2))), int(rick.yPos - (22 * sin(rick.ang2)))), 3, 0)
 
         #Sprite
         screen.blit(rot_center(sprite, degrees(rick.angle)), (rick.xPos - 20, rick.yPos - 22))
